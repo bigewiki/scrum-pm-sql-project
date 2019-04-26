ec8d8f3ecf2dfa9d8dfe4ccf9208a9c367a73d7b
Edward M <edwardtmuniz@gmail.com>
2019-04-25 20:11:08 -0700

    added an event to create a future sprint every 2 weeks

--- events.sql
+++ events.sql
@@ -1,8 +1,5 @@
--- -- example event
---
-
--- CREATE EVENT expire_events
--- ON SCHEDULE EVERY 5 MINUTE
---  DO
---  DELETE FROM events_sessions
---  WHERE stime < CURRENT_TIMESTAMP - INTERVAL 1 DAY;
+-- drop event autoCreateFutureSprint;
+CREATE EVENT autoCreateFutureSprint
+ON SCHEDULE EVERY 2 WEEK
+ DO
+ call createFutureSprint();
