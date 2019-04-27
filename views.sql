04afe53bc4cabc1e9e18c85874c2295bbb006520
Edward M <edwardtmuniz@gmail.com>
2019-04-27 00:10:10 -0700

    commented out select tets in views.sql

--- views.sql
+++ views.sql
@@ -4,7 +4,7 @@ Create algorithm = temptable view story_status AS
 SELECT s.name,s.status,CONCAT(u.fname,' ',u.lname) AS 'owner'
 FROM stories AS s JOIN users u ON (s.owner=u.id);
 -- view the temp table:
-SELECT * from story_status;
+-- SELECT * from story_status;
 
 
 -- create another temptable for all users and their roles
@@ -13,4 +13,4 @@ Create algorithm = temptable view user_roles AS
 SELECT CONCAT(u.fname,' ',u.lname) AS 'user',r.role_name
 FROM users AS u JOIN roles r ON (u.role_id=r.id);
 -- view the temp table:
-SELECT * from user_roles;
+-- SELECT * from user_roles;
