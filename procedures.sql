2a91c508b28bec33d764136384aef1bc5e9a93f6
Edward Muniz <31876255+bigewiki@users.noreply.github.com>
2019-11-18 21:35:35 -0700

    updated createKey to give 20 minutes on expiration

--- procedures.sql
+++ procedures.sql
@@ -259,6 +259,6 @@ CREATE PROCEDURE createKey(userId INT, newKey VARCHAR(255))
 BEGIN
     DELETE FROM api_keys WHERE id = userId;
     INSERT INTO api_keys (`user`,`creation`,`expiration`,`value_hash`)
-    VALUES (userId,NOW(),(NOW() + INTERVAL 5 MINUTE),newKey);
+    VALUES (userId,NOW(),(NOW() + INTERVAL 20 MINUTE),newKey);
 END EOF
 delimiter ;
